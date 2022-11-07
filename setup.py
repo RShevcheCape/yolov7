50b8380deb693fd5f2f3ae6c1852e7771b8086ff
RShevcheCape <rostyslav@capeanalytics.com>
2022-11-07 15:46:49 +0100

    setup

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
     description="Implementation of paper - YOLOv7: Trainable bag-of-freebies sets new state-of-the-art for real-time object detectors",
     scripts=glob.glob("scripts/*"),
     # This tells setuptools to include the files contained in the MANIFEST.in file
-    include_package_data=True,
+    #include_package_data=True,
     # This avoids the creation of a wheel
-    zip_safe=False,
+    #zip_safe=False,
 )
 
